a0b0f7e027e80557b7eb332d9529747c3e529cce
MatteoLoddo <loddomatteo96@gmail.com>
2022-05-19 10:44:15 +0200

    fix  string to Numb er with parseInt

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let numeroKmDaPercorrere = prompt("Quanti Km deve percorrere?");
-let etaPasseggero = prompt("Quanti anni ha?");
+const numeroKmDaPercorrere = parseInt(prompt("Quanti Km deve percorrere?"));
+const etaPasseggero = parseInt(prompt("Quanti anni ha?"));
 const prezzoAlKm = (0.21);
 // const calcoloPrezzoBiglietto =(numeroKmDaPercorrere * prezzoAlKm)
 const calcoloPerMinorenni = (prezzoAlKm * numeroKmDaPercorrere / 100 * 20)
@@ -8,7 +8,7 @@ const calcoloPerOver65 = (prezzoAlKm * numeroKmDaPercorrere / 100 * 40)
 const attention = ("<span>Attenzione! Il prezzo del biglietto non e stato calcolato correttamente, per favore inserire correttamente i dati</span>")
 
 
-const prezzoBigliettoStandard = (numeroKmDaPercorrere * prezzoAlKm).toFixed(2)
+const prezzoBigliettoStandard = parseInt(( numeroKmDaPercorrere * prezzoAlKm).toFixed(2))
 const prezzoBigliettoMinori = ( prezzoBigliettoStandard - calcoloPerMinorenni).toFixed(2)
 const prezzoBigliettoOver65 = ( prezzoBigliettoStandard - calcoloPerOver65).toFixed(2)
 
@@ -27,17 +27,17 @@ if (etaPasseggero <= 17){
     document.getElementById("eta-pass").innerHTML = (`Eta Passeggero ${etaPasseggero} anni`);
     document.getElementById("km-percorrere").innerHTML = (`Km ${numeroKmDaPercorrere}`);
     document.getElementById("prezzo-finale").innerHTML = (prezzoBigliettoMinori + "€");
-    console.log(prezzoBigliettoMinori)
+    // console.log(prezzoBigliettoMinori)
 }else if( etaPasseggero >= 65){
     document.getElementById("eta-pass").innerHTML = (`Eta Passeggero ${etaPasseggero} anni`);
     document.getElementById("km-percorrere").innerHTML = (`Km ${numeroKmDaPercorrere}`);
     document.getElementById("prezzo-finale").innerHTML = (prezzoBigliettoOver65 + "€");
-    console.log(prezzoBigliettoOver65)
+    // console.log(prezzoBigliettoOver65)
 }
-else if(etaPasseggero){
+else{
     document.getElementById("eta-pass").innerHTML = (`Eta Passeggero ${etaPasseggero} anni`);
     document.getElementById("km-percorrere").innerHTML = (`Km ${numeroKmDaPercorrere}`);
     document.getElementById("prezzo-finale").innerHTML = (prezzoBigliettoStandard + "€");
-    console.log(prezzoBigliettoStandard)
+    // console.log(prezzoBigliettoStandard)
 }
 
